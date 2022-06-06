3bd48cbbcefc92070308363422fbacb6dd8d2758
sharifok1 <sharifok1@gmail.com>
2022-06-06 10:01:00 +0600

    remove env

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const { MongoClient } = require("mongodb");
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-// const  ObjectId = require('mongodb').ObjectId;
+
 const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.he93e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://MultiForm:95W9Miamgz2D2ozx@cluster0.he93e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// console.log(uri)
+
 
 async function run(){
   try {
